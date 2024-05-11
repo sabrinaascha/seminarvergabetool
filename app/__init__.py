a2848d3fe2d1034a42faf69bdf9082cea3ee866a
sabrinaascha <107704699+sabrinaascha@users.noreply.github.com>
2024-05-11 19:40:18 +0200

    Building the initial Project - setup

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote_plus
+
+app = Flask(__name__)
+
+
+
+# Datenbank-Konfiguration
+db_user = 'root'
+db_password = '2024'
+db_coded_password = quote_plus(db_password)
+db_host = 'localhost'
+db_database = 'seminarvergabetool'
+
+# SQLAlchemy Datenbank URI konfigurieren
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{db_user}:{db_coded_password}@{db_host}/{db_database}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Um zusätzliche Warnungen zu vermeiden
+
+# Sicherheitsschlüssel für Sessions und Cookies
+#app.config["SECRET_KEY"] = "DeinGeheimerSchlüssel"  # Ersetze dies durch einen tatsächlichen geheimen Schlüssel
+
+
+# Initialisiere SQLAlchemy mit der Flask-Anwendung --> creation in models.py
+#db = SQLAlchemy(app)
+
+
+#blueprints importieren + registrieren
+from app.views.index import bp_index
+
+app.register_blueprint(bp_index, url_prefix="")
+
+
+
---
+++ app/models.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, date, timedelta
+
+db = SQLAlchemy()
